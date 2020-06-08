e0ff35f33705e779bd98d790a45c37eaad1ca818
Khalid Mohamud <kmohamud@zipwhip.com>
2020-06-07 18:04:25 -0700

    formatted code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-/// global v ariables
+/// global variables
 var order = [];
 var list = document.getElementById('coffeeList');
 var blackCoffee = document.getElementById('black')
@@ -14,84 +14,84 @@ var full = document.getElementById('full')
 var menu = document.getElementById('menu');
 
 //constructor function for items
-function Item (name, id) {
+function Item(name, id) {
   this.name = name;
   this.id = id;
   this.amount = 0;
   order.push(this);
 }
 
-var black = new Item ('BLACK COFFEE' ,blackCoffee.id);
-var french = new Item ('FRENCH VANILLA COFFEE' ,frenchCoffee.id);
-var chocolate = new Item ('CHOCOLATE COFFEE' ,chocolateCoffee.id);
-var white = new Item ('WHITE CHOCOLATE COFFEE' ,whiteChocolateCoffee.id);
-var caramel = new Item ('CARAMEL COFFEE' ,caramelCoffee.id);
+var black = new Item('BLACK COFFEE', blackCoffee.id);
+var french = new Item('FRENCH VANILLA COFFEE', frenchCoffee.id);
+var chocolate = new Item('CHOCOLATE COFFEE', chocolateCoffee.id);
+var white = new Item('WHITE CHOCOLATE COFFEE', whiteChocolateCoffee.id);
+var caramel = new Item('CARAMEL COFFEE', caramelCoffee.id);
 
-items.textContent = 'ORDER  ' + numberOfItems ;
+items.textContent = 'ORDER  ' + numberOfItems;
 
-function clicked (event) {
+function clicked(event) {
   event.preventDefault();
   var id = event.target.id;
-  for(var i = 0; i < order.length; i++){
+  for (var i = 0; i < order.length; i++) {
     if (id === order[i].id) {
       numberOfItems++;
-      items.textContent = 'ORDER  ' + numberOfItems ;
+      items.textContent = 'ORDER  ' + numberOfItems;
       order[i].amount++;
-      if(!buyItem.includes(order[i]) ){
-         buyItem.push(order[i]);
-        }
+      if (!buyItem.includes(order[i])) {
+        buyItem.push(order[i]);
       }
-    } 
-    
+    }
   }
+
+}
 
 
 function showOrder() {
   removeMenu();
-  for(var i = 0; i < buyItem.length; i++){
-  if(buyItem[i].amount >0) {
-    // creates element for the DOM
-    var div = document.createElement('div');
-    var li = document.createElement('li');
-    // Set attributes of created elements
-    li.setAttribute('id',buyItem[i].name);
-    div.setAttribute('class', 'item')
-    li.textContent = buyItem[i].name + ' ' + buyItem[i].amount; 
-    // appends elements tpo the DOM
-    div.appendChild(li);
-    full.appendChild(div)  
-    }       
+  for (var i = 0; i < buyItem.length; i++) {
+    if (buyItem[i].amount > 0) {
+      // creates element for the DOM
+      var div = document.createElement('div');
+      var li = document.createElement('li');
+      // Set attributes of created elements
+      li.setAttribute('id', buyItem[i].name);
+      div.setAttribute('class', 'item')
+      li.textContent = buyItem[i].name + ' ' + buyItem[i].amount;
+      // appends elements to the DOM
+      div.appendChild(li);
+      full.appendChild(div)
+    }
   }
 }
 
-function removeOrder () {
+function removeOrder() {
   // remove child from the DOM
-  full.removeChild(full.childNodes[full.childElementCount] );
+  full.removeChild(full.childNodes[full.childElementCount]);
   list.style.display = 'block'
 }
 
-function removeMenu () {
+function removeMenu() {
   full.style.display = 'block';
 }
 
-function removeItem (event) {
+function removeItem(event) {
   event.preventDefault()
   var id = event.target.id;
-  for (var i = 0; i < buyItem.length ; i++) {
+  for (var i = 0; i < buyItem.length; i++) {
     if (id === buyItem[i].name) {
-      numberOfItems= numberOfItems - buyItem[i].amount;
-      buyItem[i].amount-=buyItem[i].amount;
+      numberOfItems = numberOfItems - buyItem[i].amount;
+      buyItem[i].amount -= buyItem[i].amount;
       // update texts on the DOM
-      items.textContent = 'ORDER  ' + numberOfItems  ;
+      items.textContent = 'ORDER  ' + numberOfItems;
       var ll = document.getElementById(buyItem[i].name);
       //removes element from the DOM
       ll.parentNode.removeChild(ll)
-      return numberOfItems;   
-     }
-  }  
+      return numberOfItems;
+    }
+  }
 }
 
-full.addEventListener('click',removeItem)
+full.addEventListener('click', removeItem)
 menu.addEventListener('click', removeOrder)
-list.addEventListener('click' , clicked)
+list.addEventListener('click', clicked)
 items.addEventListener('click', showOrder)
